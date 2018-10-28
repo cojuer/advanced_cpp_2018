cdda30ce4fff4429a4d46328ae2a8daf4a0e8cfa
cojuer <cojuer@gmail.com>
2018-10-28 17:56:40 +0300

    Add solution for task 1

---
+++ task_1/tree.h
@@ -0,0 +1,120 @@
+#pragma once
+
+#include <memory>
+
+namespace bintree {
+    template <typename T>
+    struct TNode : std::enable_shared_from_this<TNode<T>> {
+        using TNodePtr = std::shared_ptr<TNode<T>>;
+        using TNodeWeakPtr = std::weak_ptr<TNode<T>>;
+        using TNodeConstPtr = std::shared_ptr<const TNode<T>>;
+
+        bool hasLeft() const {
+            return bool(left);
+        }
+
+        bool hasRight() const {
+            return bool(right);
+        }
+
+        bool hasParent() const {
+            return bool(parent);
+        }
+
+        T& getValue() {
+            return value;
+        }
+
+        const T& getValue() const {
+            return value;
+        }
+
+        TNodePtr getLeft() {
+            return left;
+        }
+
+        TNodeConstPtr getLeft() const {
+            return left;
+        }
+
+        TNodePtr getRight() {
+            return right;
+        }
+
+        TNodeConstPtr getRight() const {
+            return right;
+        }
+
+        TNodePtr getParent() {
+            return parent.lock();
+        }
+
+        TNodeConstPtr getParent() const {
+            return parent.lock();
+        }
+
+        static TNodePtr createLeaf(T v) {
+            auto obj = new TNode(v);
+            auto ptr = std::shared_ptr<TNode>(obj);
+            return ptr;
+        }
+
+        static TNodePtr fork(T v, TNode* left, TNode* right) {
+            auto obj = new TNode(v, left, right);
+            auto ptr = std::shared_ptr<TNode>(obj);
+            setParent(ptr->getLeft(), ptr);
+            setParent(ptr->getRight(), ptr);
+            return ptr;
+        }
+
+        TNodePtr replaceLeft(TNodePtr l) {
+            setParent(l, this->shared_from_this());
+            setParent(left, nullptr);
+            std::swap(l, left);
+            return l;
+        }
+
+        TNodePtr replaceRight(TNodePtr r) {
+            setParent(r, this->shared_from_this());
+            setParent(right, nullptr);
+            std::swap(r, right);
+            return r;
+        }
+
+        TNodePtr replaceRightWithLeaf(T v) {
+            return replaceRight(createLeaf(v));
+        }
+
+        TNodePtr replaceLeftWithLeaf(T v) {
+            return replaceLeft(createLeaf(v));
+        }
+
+        TNodePtr removeLeft() {
+            return replaceLeft(nullptr);
+        }
+        TNodePtr removeRight() {
+            return replaceRight(nullptr);
+        }
+
+    private:
+        T value;
+        TNodePtr left = nullptr;
+        TNodePtr right = nullptr;
+        TNodeWeakPtr parent;
+
+        TNode(T v)
+            : value(v)
+        {}
+
+        TNode(T v, TNode* left, TNode* right)
+            : value(v)
+            , left(left)
+            , right(right)
+        {}
+
+        static void setParent(TNodePtr node, TNodePtr parent) {
+            if (node)
+                node->parent = parent;
+        }
+    };
+}
